e5d81948a4b1f1852fd28ba1588af077550793cd
robert <robertrenbysanjuan@gmail.com>
2025-07-24 19:47:50 +0800

    feat: add logout confirmation dialog in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { MdOutlineLightMode, MdLogout  } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 export default function Header(){
 
+    const[showValidation, setShowValidation] = useState(false)
     const navigate = useNavigate()
 
     function logout(){
@@ -19,8 +21,21 @@ export default function Header(){
             <div className="flex gap-5">
                 <MdOutlineLightMode className="w-5 h-5 cursor-pointer transition-colors duration-200 ease-in-out hover:text-gray-300"/>
                 <CgProfile className="w-5 h-5 cursor-pointer transition-colors duration-200 ease-in-out hover:text-gray-300"/>
-                <MdLogout onClick={logout} className="w-5 h-5 cursor-pointer transition-colors duration-200 ease-in-out hover:text-gray-300"/>
+                <MdLogout onClick={()=>{setShowValidation(true)}} className="w-5 h-5 cursor-pointer transition-colors duration-200 ease-in-out hover:text-gray-300"/>
             </div>
+            
+            {
+                showValidation ? 
+                <div className="fixed inset-0 bg-[rgba(0,0,0,0.22)] flex items-center justify-center z-20 ">
+                    <div className="flex flex-col gap-10 bg-white p-4 rounded-md">
+                        <h1 className="text-black">Are you sure, you want to logout?</h1>
+                        <div className="flex gap-2 justify-between">
+                            <button onClick={()=>{setShowValidation(false)}} className="bg-gray-700 text-white py-1 px-4 rounded-[3px] cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-400 hover:text-gray-800">cancel</button>
+                            <button onClick={logout} className="bg-gray-700 text-white py-1 px-4 rounded-[3px] cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-400 hover:text-gray-800">logout</button>
+                        </div>
+                    </div>
+                </div> : ''
+            }
         </header>
     )
 }
